1ac50b159daea09c8ac7b2578484f2a08ae7810f
Eli23q <158518031+Eli23q@users.noreply.github.com>
2025-11-23 22:12:18 -0500

    Grupo 3.R

--- R/glecuyer.R
+++ R/glecuyer.R
@@ -1,28 +1,38 @@
-#' @title Combined Random Number Generators
+#' @title L’Ecuyer 1988 Pseudo-Random Number Generator
 #'
 #' @description
-#' Implementación del generador combinado de L'Ecuyer para producir números
-#' pseudoaleatorios uniformes en (0,1). Utiliza dos generadores congruenciales
-#' multiplicativos que se combinan para mejorar el periodo y las propiedades estadísticas.
+#' Implementation of L'Ecuyer's combined generator to
+#' produce pseudo-random numbers uniformly distributed
+#' in (0,1). It uses two multiplicative congruential generators
+#' that are combined to improve the period and statistical properties.
 #'
 #' @usage
 #' glecuyer(n = 1, s1 = 12345, s2 = 67890,
 #'          m1 = 2147483563, a1 = 40014,
 #'          m2 = 2147483399, a2 = 40692)
 #'
-#' @param n Cantidad de números aleatorios a generar.
-#' @param s1 Semilla inicial del primer generador.
-#' @param s2 Semilla inicial del segundo generador.
-#' @param m1 Módulo del primer generador.
-#' @param a1 Multiplicador del primer generador.
-#' @param m2 Módulo del segundo generador.
-#' @param a2 Multiplicador del segundo generador.
+#' @param n Number of random numbers to generate.
+#' @param s1 Initial seed of the first generator.
+#' @param s2 Initial seed of the second generator.
+#' @param m1 Modulus of the first generator.
+#' @param a1 Multiplier of the first generator.
+#' @param m2 Modulus of the second generator.
+#' @param a2 Multiplier of the second generator.
 #'
-#' @return Una lista con dos elementos:
+#' @return A list with two elements:
 #' \describe{
-#'   \item{numeros}{Vector de números aleatorios uniformes en (0,1).}
-#'   \item{semillas_finales}{Vector con las semillas actualizadas \code{s1} y \code{s2}.}
+#'   \item{numeros}{Vector of uniformly distributed random numbers in (0,1).}
+#'   \item{semillas_finales}{Vector containing the updated seeds \code{s1} and
+#'   \code{s2}.}
 #' }
+#'
+#' @examples
+#'
+#' res2 <- glecuyer(n = 2, s1 = 12345, s2 = 67890)
+#' sprintf("%.12f", res2$numeros)
+#' res10000 <- glecuyer(n = 10000, s1 = 987654, s2 = 123456)
+#' sprintf("%.12f", res10000$numeros)
+#'
 #' @export
 glecuyer <- function(n = 1, s1 = 12345, s2 = 67890, m1 = 2147483563, a1 = 40014, m2 = 2147483399, a2 = 40692) {
   q1 <- m1 %/% a1; r1 <- m1 %% a1
@@ -41,4 +51,7 @@ glecuyer <- function(n = 1, s1 = 12345, s2 = 67890, m1 = 2147483563, a1 = 40014,
   }
   list(numeros = uniforms, semillas_finales = c(s1 = s1, s2 = s2))
 }
+
+
+
 
