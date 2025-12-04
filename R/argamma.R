539e0499e32adffbaa6812344e632a20a4286436
johaoj19 <johaodominick217@gmail.com>
2025-12-03 20:17:52 -0500

    Grupo3

--- R/argamma.R
+++ R/argamma.R
@@ -1,11 +1,11 @@
 #' @title Acceptance and rejection method for the GAMMA distribution
 #'
 #' @description
-#' Implementation of the **Acceptance–Rejection method** to generate random
-#' numbers from a **Gamma(k, θ)** distribution.
-#' The algorithm uses a **Uniform(0, b)** proposal distribution, where
+#' Implementation of the Acceptance–Rejection method to generate random
+#' numbers from a Gamma(k, θ) distribution.
+#' The algorithm uses a Uniform(0, b) proposal distribution, where
 #' \code{b} corresponds to the 0.999 quantile of the Gamma distribution, and
-#' constructs a **constant majorizing function** based on the maximum value of
+#' constructs a constant majorizing function based on the maximum value of
 #' the Gamma density at its mode \code{xmax = (k - 1) * theta}.
 #'
 #' Each candidate \code{x} generated from the Uniform proposal is accepted with
@@ -15,41 +15,39 @@
 #' Gamma distribution.
 #'
 #' @usage
-#' argamma(m, k, theta)
+#' argamma(n, k, theta)
 #'
-#' @param m Number of random values to generate.
+#' @param n Number of random values to generate.
 #' @param k Shape parameter of the Gamma distribution.
 #' @param theta Scale parameter of the Gamma distribution.
 #'
 #' @return
-#' A numeric vector of length \code{m} containing random values generated
+#' A numeric vector of length \code{n} containing random values generated
 #' from a Gamma(k, θ) distribution.
 #'
 #' @examples
-#' # Generate 5 values from a Gamma(3, 2) distribution
-#' argamma(5, k = 3, theta = 2)
-#'
-#' # Generate 10,000 values and compare with the theoretical density
-#' x <- argamma(10000, k = 4, theta = 1)
-#' hist(x, freq = FALSE, breaks = 40)
-#' curve(dgamma(x, 4, 1), add = TRUE, col = "red", lwd = 2)
+#' # Generate 100 values from a Gamma(3, 2) distribution
+#' argamma(100, k = 3, theta = 2)
 #'
 #' @export
-argamma <- function(m, k, theta){
-  b <- qgamma(0.999, shape = k, scale = theta)
-  xmax <- (k - 1) * theta
-  mconst <- dgamma(xmax, shape = k, scale = theta)
-  ma <- mconst + 0.01
-  res <- numeric(m)
-  i <- 1
-  while(i <= m){
-    u <- glewis(2)
-    x <- u[1] * b
-    acept <- dgamma(x, k, theta) / mconst
-    if(u[2] <= acept){
-      res[i] <- x
+argamma <- function(n, k, theta) {
+  x_max <- (k - 1) * theta
+  f_max <- dgamma(x_max, shape = k, scale = theta)
+  dominio_max <- qgamma(0.999, shape = k, scale = theta)
+  f_objetivo <- function(x) {
+    dgamma(x, shape = k, scale = theta)
+  }
+  muestras <- numeric(n)
+  i <- 0
+  while (i < n) {
+    u1 <- glewis(1)
+    u2 <- glewis(1)
+    x <- u1 * dominio_max
+    P_aceptar <- f_objetivo(x) / f_max
+    if (u2 <= P_aceptar) {
       i <- i + 1
+      muestras[i] <- x
     }
   }
-  res
+  return(muestras)
 }
